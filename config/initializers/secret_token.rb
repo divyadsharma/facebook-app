710a0acfa6e724a470921bbce6302a4b4443284f
divyadsharma <dvsharma129@gmail.com>
2016-12-13 08:30:21 +0530

    added user sign in/signup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FacebookApp::Application.config.secret_key_base = 'fc98aa48246607561163a61230a5f223ab4c1183b7d0716285f9cecb2df8f0d0ae866da8abdbb7f98489835fb11d06e9686f12817aeea34532b68093fc779831'
