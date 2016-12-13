95a913ae7b435c6b45d32b58b797d29e71be880a
divyadsharma <dvsharma129@gmail.com>
2016-12-13 23:00:57 +0530

    Added devise setup for registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:username, :email, :password, :password_confirmation, :remember_me)
+    end
+  end
+
+  private
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+
+  # Overwriting the sign_out redirect path method
+  # def after_sign_out_path_for(resource_or_scope)
+  #   root_path
+  # end
 end
---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,37 @@
+class RegistrationsController < Devise::RegistrationsController
+  def new
+    user = User.find_by_email(params[:email])
+    # session[:user_return_to] = params[:token] unless params[:token].blank?
+    # session[:course_token] = params[:token] unless params[:token].blank?
+    if user
+      redirect_to new_user_session_path(token: params[:token], error: I18n.t(
+          'access_denied')
+                                         )
+    else
+      binding.pry
+      build_resource(email: params[:email])
+      respond_with resource
+    end
+  end
+
+  def create
+    binding.pry
+    if params[:user].present? && params[:user][:password].present?
+      params[:user][:password] = params[:user][:password_confirmation]
+    end
+    user = User.create user_params
+    if user.errors.present?
+      flash[:alert] = 'sorry'
+      redirect_to new_user_registration_path
+    else
+      flash.now[:notice] = 'signed up'
+      session['uid'] = user.id
+      sign_in(user)
+      redirect_to root_path
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+end
