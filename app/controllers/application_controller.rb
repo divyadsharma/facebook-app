13beef2136742026f0ad9cf1f4948672e5e6b026
divya sharma <divya.sharma@weboniselab.com>
2016-12-21 14:46:15 +0530

    Added authentication without devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  include SessionsHelper
+  # before_action :authenticate_user!
+  # before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
@@ -16,9 +17,9 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def after_sign_in_path_for(resource)
-    root_path
-  end
+  # def after_sign_in_path_for(resource)
+  #   root_path
+  # end
 
 
   # Overwriting the sign_out redirect path method
